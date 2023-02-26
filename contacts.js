5adf5f59c9776c9982b62c62ce5c7a5134c29529
MOkhten <m.okhten@gmail.com>
2023-02-26 11:14:35 +0200

    contacts

---
+++ contacts.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs').promises;
+
+const contactsPath = path.resolve('./db/contacts.json');
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
