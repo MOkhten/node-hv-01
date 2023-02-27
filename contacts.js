26ec1b9797d5574467657d09f0c61dd13cc26a8d
MOkhten <m.okhten@gmail.com>
2023-02-27 10:57:43 +0200

    contacts functions

--- contacts.js
+++ contacts.js
@@ -1,20 +1,35 @@
 const path = require('path');
 const fs = require('fs').promises;
+const { uid } = require('uid');
 
 const contactsPath = path.resolve('./db/contacts.json');
 
-function listContacts() {
-  // ...твой код
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+ try {
+    const contacts = await listContacts();
+     const result = contacts.find(({id}) => id === contactId);
+    return result || null;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const changeContact = contacts.filter(e => e.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(changeContact));
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
 }
